b3f6f175733243fa030ffb7fd69ff123a3d4bec3
Wildhoney <adam.timberlake@gmail.com>
2020-12-12 18:29:58 +0000

    Implemented the scrolling behaviour

--- src/components/Sticky/index.js
+++ src/components/Sticky/index.js
@@ -1,50 +1,82 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useRef } from "react";
 import PropTypes from "prop-types";
-import { useGetSet, useMeasure } from "react-use";
+import { useGetSet, useMeasure, useScrollbarWidth } from "react-use";
 
 export default function Sticky({ children, ...props }) {
-  const inner = useRef();
+  const scroller = useRef();
   const [get, set] = useGetSet(0);
+  const scrollBarSize = 15 ?? useScrollbarWidth();
   const [outer, { width: outerWidth }] = useMeasure();
-  const [innerWidth, setInnerWidth] = useState(0);
+  const [inner, { width: innerWidth }] = useMeasure();
 
-  useEffect(() => {
-    const width = window
-      .getComputedStyle(inner.current.firstChild)
-      .getPropertyValue("width");
+  const handleWheel = useCallback(
+    ({ deltaX, deltaY }) => {
+      document.body.style.overscrollBehavior = "none";
 
-    setInnerWidth(parseInt(width));
-  }, [outerWidth]);
+      if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        const x = get() - deltaX;
+        const d = innerWidth - outerWidth;
+        const scrollPosition = x > 0 ? 0 : Math.abs(x) >= d ? -d : x;
+        scroller.current.scrollLeft = Math.abs(scrollPosition);
+        set(scrollPosition);
+      }
+    },
+    [innerWidth, outerWidth]
+  );
+
+  const handleScroll = useCallback(() => set(-scroller.current.scrollLeft), []);
+
+  const handleMouseEnter = useCallback(
+    () => (document.body.style.overscrollBehavior = "none"),
+    []
+  );
+
+  const handleMouseLeave = useCallback(
+    () => (document.body.style.overscrollBehavior = "auto"),
+    []
+  );
 
   return (
     <div
       ref={outer}
       {...props}
-      style={{ contain: "content", border: "2px solid red" }}
-      onWheel={(e) => {
-        document.body.style.overscrollBehavior = "none";
-        if (Math.abs(e.deltaX) > Math.abs(e.deltaY)) {
-          const x = get() - e.deltaX;
-          const d = innerWidth - outerWidth;
-          console.log(Math.abs(x), d);
-          set(x > 0 ? 0 : Math.abs(x) >= d ? -d : x);
-        }
-      }}
-      onMouseEnter={() => (document.body.style.overscrollBehavior = "none")}
-      onMouseLeave={() => (document.body.style.overscrollBehavior = "auto")}
+      style={{ contain: "content" }}
+      onWheel={handleWheel}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <div
         ref={inner}
         style={{
           transform: `translate(${get()}px, 0)`,
+          width: "max-content",
         }}
       >
         {children}
+
+        <div
+          ref={scroller}
+          style={{
+            overflowX: "scroll",
+            overflowY: "hidden",
+            height: `${scrollBarSize}px`,
+            width: `${outerWidth}px`,
+            marginTop: `-${scrollBarSize}px`,
+            transform: `translate(${Math.abs(get())}px, 0)`,
+          }}
+          onScroll={handleScroll}
+        >
+          <div
+            style={{ width: `${innerWidth}px`, border: "1px solid white" }}
+          />
+        </div>
       </div>
     </div>
   );
 }
 
-Sticky.propTypes = { children: PropTypes.node };
+Sticky.propTypes = {
+  children: PropTypes.node,
+};
 
 Sticky.defaultProps = { children: null };
